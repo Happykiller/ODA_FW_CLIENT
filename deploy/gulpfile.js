5404c85518301cad6b05ed2df3a041702cde617f
Rosito Fabrice <fabrice.rosito@gmail.com>
2015-07-11 19:51:08 +0200

    Init

--- deploy/gulpfile.js
+++ deploy/gulpfile.js
@@ -11,16 +11,23 @@ var plumber = require('gulp-plumber');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var replace = require('gulp-replace');
+var gulp = require('gulp-param')(require('gulp'), process.argv);
 
+var OdaGulpConfig = {
+    "vendorName" : "bower_components"
+};
 
-gulp.task('deploy-mini', function(){
-    return "HelloWorld";
+gulp.task('params', function(vendorName) {
+    if(vendorName !== null){
+        OdaGulpConfig.vendorName = vendorName;
+    }
+    return;
 });
 
-gulp.task('deploy-mini', function(){
-    return "HelloWorld";
-});
-
-gulp.task('deploy-mini', function(){
-    return "HelloWorld";
+//ex : gulp install-full --vendorName libs
+gulp.task('install-full', ['params'], function() {
+    gulp.src(['full/index.html'])
+        .pipe(replace(/vendor/g, OdaGulpConfig.vendorName))
+        .pipe(gulp.dest('./../../../'));
+    return;
 });
