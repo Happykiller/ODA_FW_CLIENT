75418cdadcc154119105812fb3a04d3688c78723
Rosito Fabrice <fabrice.rosito@gmail.com>
2015-07-11 18:32:29 +0200

    Init

---
+++ deploy/gulpfile.js
@@ -0,0 +1,18 @@
+/* jshint node:true */
+
+'use strict';
+
+var gulp = require('gulp');
+var bower = require('gulp-bower');
+var connect = require('gulp-connect');
+var jshint = require('gulp-jshint');
+var launch = require('gulp-open');
+var plumber = require('gulp-plumber');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var replace = require('gulp-replace');
+
+
+gulp.task('deploy', function(){
+    return "HelloWorld";
+});
